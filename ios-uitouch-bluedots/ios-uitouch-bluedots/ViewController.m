0df3bd39e18c70f4e566f633346b467461f12f7c
Adam Wulf <adam.wulf@gmail.com>
2014-12-23 15:50:40 -0600

    simple empty app project

---
+++ ios-uitouch-bluedots/ios-uitouch-bluedots/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ios-uitouch-bluedots
+//
+//  Created by Adam Wulf on 12/23/14.
+//  Copyright (c) 2014 Milestone Made. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
